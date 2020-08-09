00cdc98406cc5ceb59cdb604be86536f98d56749
Shaswat Swarup <shaswatswarup.94@gmail.com>
2020-08-09 22:14:20 +0530

    Updates

---
+++ precache-manifest.8b6c5b8761846b812c7ba77505acc859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e762ade5de40b58f2a80cccae1245e27",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "796be25c058887febad7",
+    "url": "/MonstersRolodex/static/css/main.b5e4335f.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/MonstersRolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796be25c058887febad7",
+    "url": "/MonstersRolodex/static/js/main.23f8e8b0.chunk.js"
+  },
+  {
+    "revision": "f8f1e1783a9b514fb26f",
+    "url": "/MonstersRolodex/static/js/runtime-main.c3ab88d8.js"
+  }
+]);
